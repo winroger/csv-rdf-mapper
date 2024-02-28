705246f3a4b2e08de0c82e6c3449db8054578e25
winroger <52461706+winroger@users.noreply.github.com>
2024-02-28 16:53:04 +0100

    added yml for build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/csv-rdf-vue/',
+  publicPath: '/csv-rdf-mapper/',
   transpileDependencies: true,
 })
