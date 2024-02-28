8538ca2afc9f34bdf46913b59e52ee9240a4b6ba
winroger <52461706+winroger@users.noreply.github.com>
2024-02-28 21:58:53 +0100

    weiterer test

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/${{ csv-rdf-mapper }}/',
+  publicPath: '',
   transpileDependencies: true,
 })
